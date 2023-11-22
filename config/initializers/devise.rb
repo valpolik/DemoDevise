7854110f9d7b55a468870918f26c396f314b4390
Valentin Polikarpov <valpolik@gmail.com>
2023-11-22 06:08:16 +0300

    password complexity : Demodevise/config/initializers/devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -286,4 +286,8 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+
+
+  config.min_password_score = 0
 end
